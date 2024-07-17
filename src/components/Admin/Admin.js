639d61716c265623894ef3ebc9facabad4b75fd9
harshithbt <harshithbt0@gmail.com>
2024-07-17 17:53:05 +0530

    fileupload functionality

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -5,11 +5,13 @@ import { makeStyles } from '@material-ui/core/styles';
 import Accordion from '@material-ui/core/Accordion';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
+import { InputLabel, Button, LinearProgress } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { ThemeContext } from '../../contexts/ThemeContext';
 import AdminEdit from './AdminEdit/AdminEdit'
-import { collection, query, getDocs, onSnapshot } from "firebase/firestore";
+import { collection, query, getDocs } from "firebase/firestore";
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { db } from "../../utils/firebaseConfig";
 import './Admin.css';
 
@@ -19,8 +21,31 @@ const Admin = () => {
     const [loggedIn, setLoggedIn] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
     const [abtData, setAbtData] = useState(null);
+    const [progressValuePic, setProgressValuePic] = useState(0)
+    const [progressValueRes, setProgressValueRes] = useState(0)
     const useStyles = makeStyles((t) => ({
+        uploadBtn: {
+            backgroundColor: theme.primary,
+            color: theme.secondary,
+            borderRadius: '30px',
+            textTransform: 'inherit',
+            textDecoration: 'none',
+            margin: '8px 0',
+            width: '100%',
+            height: '50px',
+            fontSize: '1rem',
+            fontWeight: '500',
+            fontFamily: 'var(--primaryFont)',
+            border: `3px solid ${theme.tertiary}`,
+            transition: '100ms ease-out',
+            '&:hover': {
+                backgroundColor: theme.secondary,
+                color: theme.tertiary,
+                border: `3px solid ${theme.tertiary}`,
+            }
+        }
     }));
+
     const classes = useStyles();
     useEffect(() => {
         setIsLoading(true)
@@ -63,24 +88,145 @@ const Admin = () => {
         return tempString
     })
 
+    const uploadProfilePicture = ((event) => {
+        const file = event.target.files[0]
+        const fileName = file.name
+        const replaceFileName = 'abt3'
+        const fileExt = fileName.split('.').pop();
+        const genaratedFileName = replaceFileName + "." + fileExt;
+        const storage = getStorage();
+        const storageRef = ref(storage, genaratedFileName);
+        const uploadTask = uploadBytesResumable(storageRef, file);
+        uploadTask.on('state_changed',
+            (snapshot) => {
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                console.log('Upload is ' + parseInt(progress) + '% done');
+                setProgressValuePic(parseInt(progress))
+                switch (snapshot.state) {
+                    case 'paused':
+                        console.log('Upload is paused');
+                        break;
+                    case 'running':
+                        console.log('Upload is running');
+                        break;
+                }
+            },
+            (error) => {
+                // Handle unsuccessful uploads
+                setProgressValuePic(0)
+            },
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    console.log('File available at', downloadURL);
+                    setProgressValuePic(0)
+                    alert("File upload completed")
+                });
+            }
+        );
+    })
+
+    const uploadResume = ((event) => {
+        const file = event.target.files[0]
+        const fileName = file.name
+        const replaceFileName = 'resume'
+        const fileExt = fileName.split('.').pop();
+        const genaratedFileName = replaceFileName + "." + fileExt;
+        const storage = getStorage();
+        const storageRef = ref(storage, genaratedFileName);
+        const uploadTask = uploadBytesResumable(storageRef, file);
+        uploadTask.on('state_changed',
+            (snapshot) => {
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                console.log('Upload is ' + parseInt(progress) + '% done');
+                setProgressValueRes(parseInt(progress))
+                switch (snapshot.state) {
+                    case 'paused':
+                        console.log('Upload is paused');
+                        break;
+                    case 'running':
+                        console.log('Upload is running');
+                        break;
+                }
+            },
+            (error) => {
+                // Handle unsuccessful uploads
+                setProgressValueRes(0)
+            },
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    console.log('File available at', downloadURL);
+                    setProgressValueRes(0)
+                    alert("File upload completed")
+                });
+            }
+        );
+    })
+
     return (
         <>
-            <div style={{width: '100%'}}>
+            <div style={{ width: '100%' }}>
+                {!isLoading && <Accordion style={{ backgroundColor: theme.primary }}>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                    >
+                        <Typography variant="h5">fileEdit</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <div className='fileEditContainer'>
+                            <div>
+                                <InputLabel >Profile picture:</InputLabel>
+                                <Button
+                                    className={classes.uploadBtn}
+                                    variant="contained"
+                                    component="label"
+                                >
+                                    Upload File
+                                    <input
+                                        type="file"
+                                        accept="image/jpg"
+                                        hidden
+                                        onChange={(event) => uploadProfilePicture(event)}
+                                    />
+                                </Button>
+                                { progressValuePic > 0 && <LinearProgress variant="determinate" value={progressValuePic} />}
+                            </div>
+                            <div>
+                                <InputLabel for="resume">Resume:</InputLabel>
+                                <Button
+                                    className={classes.uploadBtn}
+                                    variant="contained"
+                                    component="label"
+                                >
+                                    Upload File
+                                    <input
+                                        type="file"
+                                        accept=".pdf"
+                                        hidden
+                                        onChange={(event) => uploadResume(event)}
+                                    />
+                                </Button>
+                                { progressValueRes > 0 &&<LinearProgress variant="determinate" value={progressValueRes} />}
+                            </div>
+                        </div>
+                    </AccordionDetails>
+                </Accordion>}
                 {!isLoading && abtData.map(abt => (
-                        <Accordion style={{ backgroundColor: theme.primary }}>
-                            <AccordionSummary
-                                expandIcon={<ExpandMoreIcon />}
-                                aria-controls="panel1a-content"
-                                id="panel1a-header"
-                            >
-                                <Typography variant="h5">{abt.head}</Typography>
-                            </AccordionSummary>
-                            <AccordionDetails>
-                                <div style={{width: '95%'}}>
+                    <Accordion style={{ backgroundColor: theme.primary }}>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1a-content"
+                            id="panel1a-header"
+                        >
+                            <Typography variant="h5">{abt.head}</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <div style={{ width: '95%' }}>
                                 <AdminEdit headData={abt.head} valueData={setFeildStringArray(abt.value)}></AdminEdit>
-                                </div>
-                            </AccordionDetails>
-                        </Accordion>
+                            </div>
+                        </AccordionDetails>
+                    </Accordion>
                 ))}
             </div>
         </>
